0c1df430d1e770a849b99d664ecd3ac429e53586
Hongzhe WANG <122885887@qq.com>
2022-09-03 18:55:16 +0200

    add stream module

---
+++ Euterpe/src/stream/Stream.cpp
@@ -0,0 +1,8 @@
+//
+// Created by hongzhe on 22-9-3.
+//
+
+#include "Stream.h"
+
+namespace euterpe {
+} // euterpe
---
+++ Euterpe/src/stream/Stream.h
@@ -0,0 +1,18 @@
+//
+// Created by hongzhe on 22-9-3.
+//
+
+#ifndef EUTERPE_STREAM_H
+#define EUTERPE_STREAM_H
+#include "../ByteArray/ByteArray.h"
+#include <memory>
+
+namespace euterpe {
+
+    class Stream {
+
+    };
+
+} // euterpe
+
+#endif //EUTERPE_STREAM_H
